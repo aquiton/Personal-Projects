16523a84f9829c9e667204b3f17b0a183fb52e16
Alex Quiton <80876135+aquiton@users.noreply.github.com>
2021-06-30 23:12:16 -0400

    Java / CSS  modifications
    Added color to positive and negative values and fixed font

--- Counter/script.js
+++ Counter/script.js
@@ -5,16 +5,29 @@ var counter = 0;
 
 decrease.addEventListener("click",function(){
     counter = counter - 1;
+    if(counter < 0){
+        document.getElementById("counter").style.color = "red";
+    }
+    if(counter == 0){
+        document.getElementById("counter").style.color = "black";
+    }
     document.getElementById("counter").innerHTML = counter;
     
 })
 
 increase.addEventListener("click",function(){
     counter = counter + 1;
+    if(counter > 0){
+        document.getElementById("counter").style.color = "green";
+    }
+    if(counter == 0){
+        document.getElementById("counter").style.color = "black";
+    }
     document.getElementById("counter").innerHTML = counter;
 })
 
 reset.addEventListener('click',function(){
     counter = 0;
+    document.getElementById("counter").style.color = "black";
     document.getElementById("counter").innerHTML = counter;
 })
