0f2577f63f2dbb77e921dd4e0457ba1c5f03f337
Alex Quiton <80876135+aquiton@users.noreply.github.com>
2021-07-15 18:02:36 -0400

    added style effects
    Added hello world hover effect
    working on explosion button

--- Nav Bar/script.js
+++ Nav Bar/script.js
@@ -1,2 +1,19 @@
-var navbar_opacity = document.getElementById("navbar_opacity");
-navbar_opacity.addEventListener()
+const button_explosion = document.getElementById("explosion");
+var clicked = false;
+
+
+button_explosion.addEventListener("click", function(){
+    button_explosion.classList.add("explode");
+    console.log("hello world");
+    clicked = true;
+   
+})
+
+button_explosion.addEventListener("mouseout", function(){
+    
+    if(clicked == true){
+        button_explosion.classList.remove("explode");
+        console.log("remove");
+        clicked = false;
+    }
+})
